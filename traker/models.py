eba8055228cef71410e73aedfda5e139ae467f61
Aryan Verma <aaryanverma751@gmail.com>
2025-11-27 23:34:44 +0530

    adding db

--- traker/models.py
+++ traker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class currentbalence(models.Model):
+    blance=models.FloatField(default=0)
+
+
+class transaction(models.Model):
+    balence=models.ForeignKey(currentbalence, on_delete=models.CASCADE)
+    amount=models.FloatField()
+    expense_type=models.CharField(max_length=10, choices=[
+            ('credit', 'credit'),
+            ('debit', 'debit')
+        ])
+    discription=models.CharField(max_length=200)
+    created_at=models.DateTimeField(auto_now=True)
+    # created_at=models.DateTimeField(auto_now_add=True)
+    
--- traker/views.py
+++ traker/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import currentbalence, transaction
 # Create your views here.
+
+
+
 def index(request):
+    if request.method =="POST":
+        discription=request.POST.get("discription")
+        amount=request.POST.get("amount")
+        
+        expense_type="debit"
+        if float(amount)<0:
+            expense_type="credit"
+        balence, _ =currentbalence.objects.get_or_create(id=1)
+        transaction_history=transaction.objects.create(amount=amount,
+                                                    expense_type=expense_type,
+                                                    balence=balence,
+                                                    discription=discription)
+        balence.blance+=float(transaction_history.amount)
+        balence.save()
+
+        print (discription,amount)
+        return redirect('/')
     return render (request, "index.html")    
